f27ed18968f7d972228e3b6abe4d6a0c4c1c5139
Denis Lozumirskiy <verkor25@gmail.com>
2021-10-13 09:54:19 +0300

    hw2 commit

--- src/com/pb/lozumirskij/hw2/Calculator.java
+++ src/com/pb/lozumirskij/hw2/Calculator.java
@@ -5,38 +5,38 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
 
-        int operator1;
-        int operator2;
-        String operation;
+        int operand1;
+        int operand2;
+        String sign;
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("add first number");
-        operator1 = scanner.nextInt();
+        operand1 = scanner.nextInt();
 
         System.out.println("add second number");
-        operator2 = scanner.nextInt();
+        operand2 = scanner.nextInt();
 
-        System.out.println("add operation");
-        operation = scanner.next();
+        System.out.println("add arithmetic operation");
+        sign = scanner.next();
 
-        switch (operation){
+        switch (sign){
             case "+" :
-                System.out.println(operator1 + operator2);
+                System.out.println(operand1 + operand2);
                 break;
             case "-" :
-                System.out.println(operator1 - operator2);
+                System.out.println(operand1 - operand2);
                 break;
             case "/" :
-                if(operator2 == 0) {
-                    System.out.println("you can't divide by zero");
+                if(operand2 == 0) {
+                    System.out.println("error, you can't divide by zero, try again");
                 }
                 else {
-                    System.out.println(operator1 / operator2);
+                    System.out.println(operand1 / operand2);
                 }
                 break;
             case "*" :
-                System.out.println(operator1 * operator2);
+                System.out.println(operand1 * operand2);
                 break;
             default:
                 System.out.println("error operation");
--- src/com/pb/lozumirskij/hw2/Interval.java
+++ src/com/pb/lozumirskij/hw2/Interval.java
@@ -1,7 +1,28 @@
 package com.pb.lozumirskij.hw2;
 
+import java.util.Scanner;
+
 public class Interval {
     public static void main(String[] args) {
+
+        int i;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("enter integer");
+        i = scanner.nextInt();
+
+        if(i >= 0 && i<= 14){
+            System.out.println("your number in range [0 -14]");
+        }else if(i >= 15 && i<= 35){
+            System.out.println("your number in range [15 - 35]");
+        }else if(i >= 36 && i<= 50){
+            System.out.println("your number in range [36 - 50]");
+        }else if(i >= 51 && i<= 100){
+            System.out.println("your number in range [51 - 100]");
+        }else{
+            System.out.println("your number not included any ranges");
+        }
 
     }
 }
